f7dd670e766eccd9e853adc1f2a81879dd7b3db2
David Ginanni <david.ginanni@gmail.com>
2018-11-28 17:27:19 +0100

    create primitive doctor

--- Command/DoctorDbCommand.php
+++ Command/DoctorDbCommand.php
@@ -2,27 +2,13 @@
 
 namespace GGGGino\SkuskuCartBundle\Command;
 
-use Allyou\ManagementBundle\Manipulator\AllyouServicesManipulator;
-use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Mapping\Table;
+use GGGGino\SkuskuCartBundle\Model\SkuskuCurrencyInterface;
 use GGGGino\SkuskuCartBundle\Service\CartManager;
 use GGGGino\SkuskuCartBundle\Service\SkuskuHelper;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Sonata\AdminBundle\Command\Validators;
-use Sonata\AdminBundle\Generator\AdminGenerator;
-use Sonata\AdminBundle\Generator\ControllerGenerator;
-use Sonata\AdminBundle\Manipulator\ServicesManipulator;
-use Sonata\AdminBundle\Model\ModelManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ChoiceQuestion;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
-use Symfony\Component\Console\Question\Question;
-use Symfony\Component\PropertyAccess\PropertyAccess;
 
 /**
  * Class CurrencyCreateCommand
@@ -30,11 +16,6 @@ use Symfony\Component\PropertyAccess\PropertyAccess;
  */
 class DoctorDbCommand extends ContainerAwareCommand
 {
-    /**
-     * @var SkuskuHelper
-     */
-    private $skuskuHelper;
-
     /**
      * {@inheritdoc}
      */
@@ -50,36 +31,28 @@ class DoctorDbCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->skuskuHelper = $this->getContainer()->get(SkuskuHelper::class);
+        $currencyText = $this->checkCurrencyOnDb();
+        $output->writeln($currencyText);
+    }
 
-        $entityClass = $input->getArgument('entity');
-        $classReflected = new \ReflectionClass($entityClass);
-        $entityInstance = new $entityClass();
-
-        $propertyAccessor = PropertyAccess::createPropertyAccessor();
-
-        $helper = $this->getHelper('question');
-
-        $reader = new AnnotationReader();
-        $classAnnotated = $reader->getClassAnnotation($classReflected, Table::class);
-
-        $classReflectedProps = $classReflected->getProperties();
-
-        foreach($classReflectedProps as $key => $value){
-            $question = new Question('Set value for (' . $value->name . '): ', false);
-
-            $response = $helper->ask($input, $output, $question);
-            if( !$response ){
-                continue;
-            }
-            $propertyAccessor->setValue($entityInstance, $value->name, $response);
-        }
-
+    /**
+     * Check if exists at least one currency
+     * Tip: ggggino_skusku:currency:create
+     *
+     * @return array(string, string)
+     */
+    private function checkCurrencyOnDb()
+    {
         /** @var EntityManager $em */
         $em = $this->getContainer()->get('doctrine')->getManager();
-        $em->persist($entityInstance);
-        $em->flush();
 
-        $output->writeln('Entity created');
+        /** @var SkuskuCurrencyInterface[] $Currencies */
+        $currencies = $em->getRepository(SkuskuCurrencyInterface::class)->findAll();
+
+        if( count($currencies) > 0 ) {
+            return 'Currency: <info> correct</info>';
+        }else{
+            return 'Currency: <error>not correct</error>';
+        }
     }
 }
